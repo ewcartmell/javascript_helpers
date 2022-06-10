4cb15e750d8ddb774476f01f126de4bd55cd6894
Ned Cartmell <ned@flexport.com>
2022-06-10 14:51:53 -0700

    remove window references in class definitions

--- deprecated/old_classes.js
+++ deprecated/old_classes.js
@@ -1,4 +1,4 @@
-window.Document = class Document {
+class Document {
   static all = []
 
   static get_document(DOCUMENT_ID) {
@@ -159,7 +159,7 @@ window.Document = class Document {
 }
 
 
-window.Shipment = class Shipment {
+class Shipment {
   static all = []
   static shipment_ids = Shipment.all.map(shipment => shipment.SHIPMENT_ID)
   static get_shipment(SHIPMENT_ID) {
@@ -205,7 +205,7 @@ window.Shipment = class Shipment {
 }
 
 
-window.Coordinator = class Coordinator {
+class Coordinator {
   static all = []
 
   static get_selected_site_coordinators(sites = []){
